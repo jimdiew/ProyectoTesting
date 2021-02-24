b04c2a20f9abd5f093b443e01a1cb2bcc00a2a49
jimdiew <jimediew@gmail.com>
2021-02-24 15:05:40 -0300

    Docs: Add second then condition

--- stepdefs.js
+++ stepdefs.js
@@ -2,7 +2,7 @@
 
 const assert = require('assert');
 const webdriver = require('selenium-webdriver');
-const { Given, When, Then } = require('cucumber');
+const { Given, When, Then, And } = require('cucumber');
 const {Builder} = webdriver;
 const urlPrefix = "https://www.olenick.com";
 
@@ -12,15 +12,20 @@ Given(/^I have visited the Olenick home page$/, {timeout:30000}, async function 
  });
 
 //When condition//      
-When(/^I click the "(.*)" header link$/, {timeout:10000}, async function (linkText) {
-await this.driver.findElement({ id: 'menu-main-menu' }).findElement({ linkText: 'IT Projects' }).click();
+When(/^I click the "(.*)" header link$/, async function (linkText) {
+await this.driver.findElement({ id: 'menu-main-menu' }).findElement({ linkText }).click();
 });
 
  // Then condition // 
-Then(/^the page titled "(.*)" with url "(.*)" should open in the same window$/, {timeout:10000}, async function (title, url) {
+Then(/^the page titled "(.*)" with url "(.*)" should open in the same window$/, async function (title, url) {
    const titlePromise = this.driver.getTitle();
    const urlPromise =  this.driver.getCurrentUrl();
    assert.strictEqual(await titlePromise, title);
    assert.strictEqual(await urlPromise, urlPrefix + url);
   });
-  
+
+  //Second Then condition//
+  Then(/^hero title is "(.*)"$/, async function (heroTitle){
+   const heroTitlePromise = this.driver.findElement({ className: 'fl-heading-text' }).getText();
+   assert.strictEqual(await heroTitlePromise, heroTitle );
+  });
