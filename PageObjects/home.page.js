5005e0b2f131e30fedf88f577f5c03d02be8f474
jimdiew <jimediew@gmail.com>
2021-03-04 16:11:55 -0300

    Docs: Comment code that should be used later in the project

--- PageObjects/home.page.js
+++ PageObjects/home.page.js
@@ -1,19 +1,22 @@
-// home.page.js
-import Page from './page'
+import Page from './Page';
 
-class HomePage extends Page {
+export default class HomePage extends Page {
+    constructor(webdriver){
+        super (webdriver);
+            this.heroTitle = 'Global Leaders in Software Quality Engineering';
+            this.url = 'https://www.olenick.com/';
+            this.title = 'Olenick: Global Leaders in Software Testing and Quality Engineering';       
+    }
 
     get heroTitle() { return $('.fl-heading-text') }
-    get headerLinks() { return $('#menu-main-menu a ') }
+  //  get headerLinks() { return $('#menu-main-menu a ') }
 
-    open() {
+    /*open() {
         super.open('home')
     }
 
     submit() {
         this.submitBtn.click()
     }
-
+*/
 }
-
-export default new HomePage()
