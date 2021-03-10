93d27b1875f4cad247e7a2cec65840f89daef505
jimdiew <jimediew@gmail.com>
2021-03-10 17:24:27 -0300

    Docs: Make the code work with super class

--- PageObjects/NewsPage.js
+++ PageObjects/NewsPage.js
@@ -2,9 +2,7 @@ import Subpage from './Subpage';
 
 class NewsPage extends Subpage {
     constructor(webdriver){
-        super (webdriver, 'https://www.olenick.com/news/', 
-        'Company News and Announcements - Olenick', 'News');
-           
+        super (webdriver, 'https://www.olenick.com/news/', title, heroTitle);  
     }
-    get heroTitle() { throw new Error('Hero title not implemented. This method is abstract'); }
+
 }
