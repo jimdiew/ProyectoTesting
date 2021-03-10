1fdc2d62a6bdfdd635c4536e5a9465c7028d7419
jimdiew <jimediew@gmail.com>
2021-03-10 17:23:49 -0300

    Docs: Make the code work with super class

--- PageObjects/ItGovernancePage.js
+++ PageObjects/ItGovernancePage.js
@@ -2,8 +2,7 @@ import Subpage from './Subpage';
 
 class ItGovernancePage extends Subpage {
     constructor(webdriver){
-        super (webdriver, 'https://www.olenick.com/service-categories/it-governance/', 
-        'IT Governance - Olenick Service Pillars', 'IT Governance');
+        super (webdriver, 'https://www.olenick.com/service-categories/it-governance/', title, heroTitle);  
     }
-    get heroTitle() { throw new Error('Hero title not implemented. This method is abstract'); }
+
 }
