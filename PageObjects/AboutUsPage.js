b61741fe6924bc1bce15cacca1eb68e56619d3cc
jimdiew <jimediew@gmail.com>
2021-03-15 13:17:25 -0300

    Docs: Change import to const. Add module.exports.

--- PageObjects/AboutUsPage.js
+++ PageObjects/AboutUsPage.js
@@ -1,7 +1,7 @@
-import Subpage from './Subpage';
+const Subpage = require('./Subpage');
 
-class AboutUsPage extends Subpage {
-    constructor(webdriver){
-                super (webdriver, 'https://www.olenick.com/about-us/', title, heroTitle);  
-    }
-}
+module.exports = class AboutUsPage extends Subpage {
+  constructor(webdriver) {
+    super(webdriver, 'https://www.olenick.com/about-us/', 'About Olenick: Over 20 Years of Experience in Software Testing and QA', 'About Olenick');
+  }
+};
