bfcf08db6dee4038d843239e8254318aa44de3b3
jimdiew <jimediew@gmail.com>
2021-03-15 13:30:16 -0300

    Docs: Change setDefaultTimeout from outside of function

--- support.js
+++ support.js
@@ -1,24 +1,21 @@
-const { setWorldConstructor } = require('cucumber')
-const {Builder, Capabilities} = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
- 
+const { setWorldConstructor, setDefaultTimeout } = require('cucumber');
+const { Builder, Capabilities } = require('selenium-webdriver');
+// const firefox = require('selenium-webdriver/firefox');
+
 class CustomWorld {
   constructor() {
     const caps = new Capabilities();
-    caps.setPageLoadStrategy("eager");
-    
-    this.driver = new Builder().withCapabilities(caps)
-    .forBrowser('firefox')
-    .build();
+    caps.setPageLoadStrategy('eager');
 
+    this.driver = new Builder().withCapabilities(caps)
+      .forBrowser('firefox')
+      .build();
   }
 }
-  
-setWorldConstructor(CustomWorld)
- 
-module.exports = function() {
- 
+
+setWorldConstructor(CustomWorld);
+setDefaultTimeout(10 * 1000);
+
+module.exports = function () {
   this.World = CustomWorld;
- 
-  this.setDefaultTimeout(30 * 1000);
 };
