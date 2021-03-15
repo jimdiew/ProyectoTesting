a1483c9aa3385db9223c2a9bf7853092915f2a62
jimdiew <jimediew@gmail.com>
2021-03-15 13:26:27 -0300

    Docs: -Delete use strict -Delete webdriver, Builder and urlPrefix const -Change imports to const -Change pageDict to PAGE_DICT - Delete old then condition

--- stepdefs.js
+++ stepdefs.js
@@ -1,47 +1,38 @@
-'use strict';
-
 const assert = require('assert');
-const webdriver = require('selenium-webdriver');
 const { Given, When, Then } = require('cucumber');
-const {Builder} = webdriver;
-const urlPrefix = "https://www.olenick.com";
 
-import HomePage from './PageObjects/HomePage';
-import ItProjectsPage from './PageObjects/ItProjectsPage';
-import ItOperationsPage from './PageObjects/ItOperationsPage';
+const HomePage = require('./PageObjects/HomePage');
+const ItProjectsPage = require('./PageObjects/ItProjectsPage');
+const ItOperationsPage = require('./PageObjects/ItOperationsPage');
+const ItGovernancePage = require('./PageObjects/ItGovernancePage');
+const NewsPage = require('./PageObjects/NewsPage');
+const ExpertisePage = require('./PageObjects/ExpertisePage');
+const AboutUsPage = require('./PageObjects/AboutUsPage');
 
 // Dictionary //
-const pageDict = {
-   'IT Projects': ItProjectsPage,
-   'IT Operations': ItOperationsPage,
-   'IT Governance': ItGovernancePage,
-   'News': NewsPage,
-   'Expertise': ExpertisePage,
-   'About': AboutUsPage
- } ;
-
+const PAGE_DICT = {
+  'IT Projects': ItProjectsPage,
+  'IT Operations': ItOperationsPage,
+  'IT Governance': ItGovernancePage,
+  News: NewsPage,
+  Expertise: ExpertisePage,
+  About: AboutUsPage,
+};
 
 // Given condition //
-Given(/^I have visited the Olenick home page$/, {timeout:30000}, async function () {
-   const page = new HomePage (this.driver);
-   await this.driver.get (urlPrefix);     
- });
-
-//When condition//      
-When(/^I click the "(.*)" header link$/, async function (linkText) {
-await this.driver.findElement({ id: 'menu-main-menu' }).findElement({ linkText }).click();
+Given(/^I have visited the Olenick home page$/, { timeout: 30000 }, async function () {
+  this.page = new HomePage(this.driver);
+  await this.page.open();
 });
 
- // Then condition // 
-Then(/^the page titled "(.*)" with url "(.*)" should open in the same window$/, async function (title, url) {
-   const titlePromise = this.driver.getTitle();
-   const urlPromise =  this.driver.getCurrentUrl();
-   assert.strictEqual(await titlePromise, title);
-   assert.strictEqual(await urlPromise, urlPrefix + url);
-  });
+// When condition//
+When(/^I click the "(.*)" header link$/, async function (linkText) {
+  await this.page.clickHeaderLink(linkText);
+});
 
-// New Then condition // 
-Then(/^page "(.*)" should open in the same window$/, async function(pageName) {
-   const pageClass =  pageDict[pageName]; 
-   const page = new pageClass (this.driver);
-}  )
+// Then condition //
+Then(/^page "(.*)" should open in the same window$/, async function (pageName) {
+  const PageClass = PAGE_DICT[pageName];
+  const page = new PageClass(this.driver);
+  assert.ok(await page.isOpen());
+});
